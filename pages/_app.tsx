0f81bd5f45a418695c80023852be9dd249842d90
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-01-22 18:26:21 -0300

    Implement container and background

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
-import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import { Flex, Container, ChakraProvider } from '@chakra-ui/react'
 import { Global } from '@emotion/react'
 import { DefaultSeo } from 'next-seo'
+import type { AppProps } from 'next/app'
 
 import { theme } from 'styles/theme'
 import { global } from 'styles/global'
@@ -12,8 +12,16 @@ export default function MyApp ({ Component, pageProps }: AppProps) {
     <>
       <DefaultSeo {...configSEO} />
       <ChakraProvider theme={theme} resetCSS>
-        <Global styles={global} />
-        <Component {...pageProps} />
+        <Flex minHeight='100vh' backgroundColor='white.100'>
+          <Container
+            as='main'
+            maxW='3xl'
+            padding={8}
+          >
+            <Global styles={global} />
+            <Component {...pageProps} />
+          </Container>
+        </Flex>
       </ChakraProvider>
     </>
   )
