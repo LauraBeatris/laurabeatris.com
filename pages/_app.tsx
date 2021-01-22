cd5b5aff924a17a7b6d018f2c4498bd9c275ebae
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-01-22 13:12:04 -0300

    Configure Chakra UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,10 @@
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function MyApp ({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
