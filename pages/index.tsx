e03f8c8e301304850a0dfbe22c9ffb5277d34e26
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-01-22 18:27:09 -0300

    Remove placeholder content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,5 @@
-import { Heading } from 'components/Heading'
-import { Paragraph } from 'components/Paragraph'
-import { HighlightLink } from 'components/HighlightLink'
-
 export default function Home () {
   return (
-    <>
-      <Heading width={346}>
-        Happy Sunday! ✌🏼
-        I'm Laura Beatris
-      </Heading>
-      <Paragraph size='md'>
-        I’m a Software Engineer and creator. I work at YLD and also run my Youtube Channel and a Podcast about programming. Teaching code and creating solutions are my favorite things in the world. 🚀
-      </Paragraph>
-      <HighlightLink size='md' href='https://twitter.com/lauradotjs'>
-        Teste
-      </HighlightLink>
-    </>
+    <></>
   )
 }
