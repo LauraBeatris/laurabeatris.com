6d6be1035645e7cc7d2469c7ca4f4fbbc08aecfb
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-01-22 15:52:05 -0300

    Configure next-seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,20 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { Global } from '@emotion/react'
+import { DefaultSeo } from 'next-seo'
 
 import { theme } from 'styles/theme'
 import { global } from 'styles/global'
+import { configSEO } from 'next-seo.config'
 
 export default function MyApp ({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme} resetCSS>
-      <Global styles={global} />
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <DefaultSeo {...configSEO} />
+      <ChakraProvider theme={theme} resetCSS>
+        <Global styles={global} />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   )
 }
