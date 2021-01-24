c47c42f92b15f177365ab63783028dc10c9a4bde
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-01-24 10:01:34 -0300

    Add popover to show additional info about projects

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,24 @@
-import { VStack, Heading } from '@chakra-ui/react'
+import {
+  VStack,
+  Popover,
+  Button,
+  PopoverBody,
+  PopoverArrow,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverCloseButton
+} from '@chakra-ui/react'
+import { InfoIcon } from '@chakra-ui/icons'
 
-import { HighlightLink } from 'components/Base/HighlightLink'
+import { Heading } from 'components/Base/Heading'
 import { Paragraph } from 'components/Base/Paragraph'
+import { HighlightLink } from 'components/Base/HighlightLink'
 import { links } from 'constants/links'
 import { getDayOfWeek } from 'utils/getDayOfWeek'
 
 const now = new Date()
 const dayOfWeek = getDayOfWeek(now.getDate(), now.getMonth(), now.getFullYear())
+const popoverText = 'Click on the projects to see more details about it'
 
 export default function Home () {
   return (
@@ -27,6 +39,31 @@ export default function Home () {
         and a <HighlightLink href={links.podcast}>Podcast</HighlightLink> about programming.
         Teaching and creating solutions are my favorite things in the world. <span role='img' aria-hidden='true'>🚀</span>
       </Paragraph>
+
+      <Heading as='h2' paddingTop={10}>
+        Projects
+        <Popover aria-label={popoverText} placement='top'>
+          <PopoverTrigger>
+            <Button
+              height='unset'
+              variant='unstyled'
+              minWidth='unset'
+              marginLeft={2}
+            >
+              <InfoIcon boxSize={5} color='green.400' />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent backgroundColor='dark'>
+            <PopoverArrow backgroundColor='dark' />
+            <PopoverCloseButton color='white.100' />
+            <PopoverBody>
+              <Paragraph variant='white' size='sm'>
+                {popoverText}
+              </Paragraph>
+            </PopoverBody>
+          </PopoverContent>
+        </Popover>
+      </Heading>
     </VStack>
   )
 }
