0228bd314489b9359e6a3b5c55475fb9c4f165bd
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-01-25 19:19:55 -0300

    Fix styles propery from theme

--- styles/theme/index.ts
+++ styles/theme/index.ts
@@ -4,16 +4,12 @@ import { colors } from './colors'
 import { fonts } from './fonts'
 import { components } from './components'
 import { config } from './config'
+import { styles } from './styles'
 
 export const theme = extendTheme({
   config,
   colors,
   fonts,
-  global: {
-    body: {
-      bg: 'var(--bg-color)',
-      color: 'var(--text-color)'
-    }
-  },
+  styles,
   components
 })
---
+++ styles/theme/styles.ts
@@ -0,0 +1,8 @@
+export const styles = {
+  global: {
+    body: {
+      bg: 'var(--bg-color)',
+      color: 'var(--text-color)'
+    }
+  }
+}
