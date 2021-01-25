d2d4a6ca9f4dfee33016abb3b85b035c3ca0b8ce
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-01-25 09:35:51 -0300

    Add types definitions of GraphQL schema

---
+++ graphql/schema.ts
@@ -0,0 +1,25 @@
+export type Stack = {
+  language: string;
+  framework?: string;
+  libraries: Array<string>;
+  categories: Array<string>;
+  databases?: Array<string>;
+}
+
+export type Project = {
+  title: string;
+  stack: Stack;
+  githubUrl: string;
+  liveUrl?: string;
+  description?: string;
+}
+
+export type Timeline = Array<{
+  id: string;
+  year: number;
+  achievements: Array<{
+    id: string;
+    title: string;
+    description?: string;
+  }>
+}>
