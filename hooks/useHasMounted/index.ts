1b4a39153aa77472766dbaf4ce86e15e17207a67
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-01-25 19:44:08 -0300

    Do not render page while hydration

---
+++ hooks/useHasMounted/index.ts
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react'
+
+/**
+ * Handles the mounted state of the component.
+ *
+ * Useful for cases where a content should only be rendered on the browser
+ * and the DOM should stay the same on the server and on the re-hydration process.
+ */
+const useHasMounted = () => {
+  const [hasMounted, setHasMounted] = useState(false)
+
+  useEffect(() => {
+    setHasMounted(true)
+  }, [])
+
+  return hasMounted
+}
+
+export default useHasMounted
