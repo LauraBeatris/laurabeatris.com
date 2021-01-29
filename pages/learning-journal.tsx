630df0309050c2f1cb36411dbae8fc68abb71345
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-01-29 19:31:40 -0300

    Apply border between learning journal element

--- pages/learning-journal.tsx
+++ pages/learning-journal.tsx
@@ -51,89 +51,91 @@ export default function LearningJournal ({
 
         <List width='full' paddingTop={5} spacing={6}>
           {
-          (formatLearningJournals ?? []).map(({
-            id,
-            date,
-            work,
-            curiosity,
-            programming
-          }) => {
-            const shouldShowWorkEntries = (work ?? []).length > 0
-            const shouldShowCuriosityEntries = (curiosity ?? []).length > 0
-            const shouldShowProgrammingEntries = (programming ?? []).length > 0
+            (formatLearningJournals ?? []).map(({
+              id,
+              date,
+              work,
+              curiosity,
+              programming
+            }, index) => {
+              const shouldShowWorkEntries = (work ?? []).length > 0
+              const shouldShowCuriosityEntries = (curiosity ?? []).length > 0
+              const shouldShowProgrammingEntries = (programming ?? []).length > 0
 
-            return (
-              <VStack
-                as='li'
-                key={id}
-                width='full'
-                spacing={5}
-                alignItems='flex-start'
-              >
-                <Text
-                  as='h3'
-                  bgClip='text'
-                  fontSize={22}
-                  fontWeight='bold'
-                  bgGradient='linear(to-r, green.400, green.500, blue.100)'
+              return (
+                <VStack
+                  as='li'
+                  key={id}
+                  width='full'
+                  spacing={5}
+                  alignItems='flex-start'
+                  paddingTop={5}
+                  borderTopWidth={1}
                 >
-                  {date}
-                </Text>
+                  <Text
+                    as='h3'
+                    bgClip='text'
+                    fontSize={22}
+                    fontWeight='bold'
+                    bgGradient='linear(to-r, green.400, green.500, blue.100)'
+                  >
+                    {date}
+                  </Text>
 
-                {
-                  shouldShowWorkEntries
-                    ? (
-                      <>
-                        <Heading size='xs'>Work</Heading>
-                        <UnorderedList paddingLeft={5}>
-                          {
-                            work.map(text => (
-                              <ListItem key={text}>{text}</ListItem>
-                            ))
-                          }
-                        </UnorderedList>
-                      </>
-                      )
-                    : null
-                }
+                  {
+                    shouldShowWorkEntries
+                      ? (
+                        <>
+                          <Heading size='xs'>Work</Heading>
+                          <UnorderedList paddingLeft={5}>
+                            {
+                              work.map(text => (
+                                <ListItem key={text}>{text}</ListItem>
+                              ))
+                            }
+                          </UnorderedList>
+                        </>
+                        )
+                      : null
+                  }
 
-                {
-                  shouldShowProgrammingEntries
-                    ? (
-                      <>
-                        <Heading size='xs'>Programming</Heading>
-                        <UnorderedList paddingLeft={5}>
-                          {
-                            programming.map(text => (
-                              <ListItem key={text}>{text}</ListItem>
-                            ))
-                          }
-                        </UnorderedList>
-                      </>
-                      )
-                    : null
-                }
+                  {
+                    shouldShowProgrammingEntries
+                      ? (
+                        <>
+                          <Heading size='xs'>Programming</Heading>
+                          <UnorderedList paddingLeft={5}>
+                            {
+                              programming.map(text => (
+                                <ListItem key={text}>{text}</ListItem>
+                              ))
+                            }
+                          </UnorderedList>
+                        </>
+                        )
+                      : null
+                  }
 
-                {
-                  shouldShowCuriosityEntries
-                    ? (
-                      <>
-                        <Heading size='xs'>Curiosity</Heading>
-                        <UnorderedList paddingLeft={5}>
-                          {
-                            curiosity.map(text => (
-                              <ListItem key={text}>{text}</ListItem>
-                            ))
-                          }
-                        </UnorderedList>
-                      </>
-                      )
-                    : null
-                }
-              </VStack>
-            )
-          })
-        }
+                  {
+                    shouldShowCuriosityEntries
+                      ? (
+                        <>
+                          <Heading size='xs'>Curiosity</Heading>
+                          <UnorderedList paddingLeft={5}>
+                            {
+                              curiosity.map(text => (
+                                <ListItem key={text}>{text}</ListItem>
+                              ))
+                            }
+                          </UnorderedList>
+                        </>
+                        )
+                      : null
+                  }
+                </VStack>
+              )
+            })
+          }
         </List>
       </VStack>
     </ChakraProvider>
