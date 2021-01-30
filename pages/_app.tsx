5fecb82e48160bddfd58696c421b03c52a2b4d84
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-01-30 18:55:06 -0300

    Do not wait for hydration to render app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,16 +4,8 @@ import type { AppProps } from 'next/app'
 
 import { global } from 'styles/global'
 import { configSEO } from 'next-seo.config'
-import useHasMounted from 'hooks/useHasMounted'
 
 export default function MyApp ({ Component, pageProps }: AppProps) {
-  // Temporary solution to avoid showing color mode after hydration process
-  const hasMounted = useHasMounted()
-
-  if (!hasMounted) {
-    return null
-  }
-
   return (
     <>
       <DefaultSeo {...configSEO} />
