3633c58000a122d448b53c3140bbdc2e5e5ed766
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-02-04 18:45:26 -0300

    Do not render ToggleThemeIcon until app has mounted

--- components/ToggleThemeButton/index.tsx
+++ components/ToggleThemeButton/index.tsx
@@ -1,13 +1,15 @@
-import { Button, ButtonProps, useColorMode } from '@chakra-ui/react'
+import { Button, ButtonProps, Skeleton, useColorMode } from '@chakra-ui/react'
 import useSound from 'use-sound'
 
 import switchOnSound from 'public/sounds/switch-on.mp3'
 import switchOffSound from 'public/sounds/switch-off.mp3'
 
 import { ToggleThemeIcon } from 'components/ToggleThemeIcon'
+import { useHasMounted } from 'hooks/useHasMounted'
 
 export function ToggleThemeButton (props: ButtonProps) {
   const { colorMode, toggleColorMode } = useColorMode()
+  const { hasMounted } = useHasMounted()
 
   const isDarkMode = colorMode === 'dark'
   const [play] = useSound(isDarkMode ? switchOnSound : switchOffSound)
@@ -20,15 +22,21 @@ export function ToggleThemeButton (props: ButtonProps) {
   const iconTitle = isDarkMode ? 'Switch to light mode' : 'Switch to dark mode'
 
   return (
-    <Button
-      onClick={handleClick}
-      display='flex'
-      variant='unstyled'
-      alignItems='center'
-      justifyContent='center'
-      {...props}
+    <Skeleton
+      isLoaded={hasMounted}
+      startColor='transparent'
+      endColor='transparent'
     >
-      <ToggleThemeIcon title={iconTitle} isDarkMode={isDarkMode} />
-    </Button>
+      <Button
+        onClick={handleClick}
+        display='flex'
+        variant='unstyled'
+        alignItems='center'
+        justifyContent='center'
+        {...props}
+      >
+        <ToggleThemeIcon title={iconTitle} isDarkMode={isDarkMode} />
+      </Button>
+    </Skeleton>
   )
 }
