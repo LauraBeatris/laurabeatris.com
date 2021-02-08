d6c4fa852b987bcee15cddba03ab201430270ab1
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-02-08 07:08:18 -0300

    Remove unused imports

--- styles/theme/components.ts
+++ styles/theme/components.ts
@@ -1,11 +1,9 @@
 import { highlightLinkStyles, HIGHLIGHT_LINK_THEME_KEY } from 'components/Base/HighlightLink/styles'
 import { headingStyles, HEADING_THEME_KEY } from 'components/Base/Heading/styles'
 import { paragraphStyles, PARAGRAPH_THEME_KEY } from 'components/Base/Paragraph/styles'
-import { unorderedListStyles, UNORDERED_LIST_THEME_KEY } from 'components/Base/UnorderedList/styles'
 
 export const components = {
   [HEADING_THEME_KEY]: headingStyles,
   [PARAGRAPH_THEME_KEY]: paragraphStyles,
-  [HIGHLIGHT_LINK_THEME_KEY]: highlightLinkStyles,
-  [UNORDERED_LIST_THEME_KEY]: unorderedListStyles
+  [HIGHLIGHT_LINK_THEME_KEY]: highlightLinkStyles
 }
