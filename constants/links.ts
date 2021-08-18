d0a121e8ac49322971785f4bc3011c2430b3e07b
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-08-18 18:52:59 -0300

    Fix link to be external

--- constants/links.ts
+++ constants/links.ts
@@ -1,5 +1,5 @@
 export const links = {
-  reaktor: 'reaktor.com',
+  reaktor: 'https://reaktor.com',
   twitter: 'https://twitter.com/lauradotjs',
   youtube: 'https://www.youtube.com/c/LauraBeatris',
   linkedin: 'https://www.linkedin.com/in/laurabeatris/',
