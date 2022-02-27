bcbf4c889ae6b3847d904fb9cacdf1b77ab54149
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2022-02-27 13:16:55 +0100

    Fix query name

--- graphql/queries/getAboutMePage.ts
+++ graphql/queries/getAboutMePage.ts
@@ -4,7 +4,7 @@ import { graphQLClient } from 'config/graphQLClient'
 import { AboutMePage } from 'graphql/schema'
 
 const GET_ABOUT_ME_PAGE_QUERY = gql`
-  query getAboutMePageSections($id: ID!){
+  query getAboutMePage($id: ID!){
     aboutMePage(where: { id: $id }){
       sections {
         id
@@ -27,9 +27,9 @@ const GET_ABOUT_ME_PAGE_QUERY = gql`
 `
 
 /**
- * Since "aboutMePage" is a contextual model, there should be
- * only one content entry for it, which at the moment relies on this ID
- * @see {@link https://graphcms.com/academy/what-is-content-modeling} for more info regarding contextual models
+ * Since "aboutMePage" is a subject model, there should be only one content
+ * entry for it, which at the moment relies on this ID
+ * @see {@link https://graphcms.com/academy/what-is-content-modeling} for more info regarding subject models
  */
 const pageIDFromCMS = 'cl04asq370z7o0ewwt2726r7t'
 
