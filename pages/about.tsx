7ae0c67769d172ad090e717e035f45158a1413b1
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2022-02-27 10:55:21 +0100

    Fetch page data on build time

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,9 +1,30 @@
 import { VStack } from '@chakra-ui/react'
 
+import { getAboutMePage } from 'graphql/queries/getAboutMePage'
+
 import { Heading } from 'components/Base/Heading'
 import { HighlightLink } from 'components/Base/HighlightLink'
 import { Paragraph } from 'components/Base/Paragraph'
 import { PlaceImage } from 'components/PlaceImage'
+
+export const getStaticProps = async () => {
+  try {
+    const { sections } = await getAboutMePage()
+
+    return {
+      props: {
+        sections
+      }
+    }
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error)
+
+    return {
+      props: {}
+    }
+  }
+}
 
 export default function About () {
   return (
