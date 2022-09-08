7994c988a3aab6802ba2c1b23e1141711a52a047
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2022-09-08 20:33:14 +0200

    Update route to get screenshot from page route

--- src/pages/api/webhooks/learning-journal.ts
+++ src/pages/api/webhooks/learning-journal.ts
@@ -1,9 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { verifyWebhookSignature } from '@graphcms/utils'
 
-import { getLearningJournalPagesCount } from 'graphql/queries/getLearningJournalPagesCount'
+import { getScreenshot } from 'config/chromium'
 
 const secret = process.env.CMS_WEBHOOK_SECRET
+const isDev = !process.env.AWS_REGION
+const isHtmlDebug = process.env.OG_HTML_DEBUG === '1'
 
 export default async function handler (
   request: NextApiRequest,
@@ -24,7 +26,18 @@ export default async function handler (
     return response.status(403).send('Invalid webhook request')
   }
 
-  const pagesCount = await getLearningJournalPagesCount()
+  const ticketImageUrl = `${process.env.NEXT_PUBLIC_APP_URL}/learning-journal/${body.data.id}`
+  if (isHtmlDebug) {
+    return response.redirect(ticketImageUrl)
+  }
 
-  return response.json({ pagesCount })
+  const file = await getScreenshot(ticketImageUrl, isDev)
+  response.statusCode = 200
+  response.setHeader('Content-Type', 'image/png')
+  response.setHeader(
+    'Cache-Control',
+    'public, immutable, no-transform, s-manage=31536000, max-age=31536000'
+  )
+
+  response.end(file)
 }
