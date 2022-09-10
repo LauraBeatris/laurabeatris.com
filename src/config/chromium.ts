0694409b99cb28152054525a31737059ee0d7db2
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2022-09-10 17:16:06 +0200

    Use Browserless instead of Puppeteer

--- src/config/chromium.ts
+++ src/config/chromium.ts
@@ -1,35 +1,13 @@
-import { Page, launch } from 'puppeteer-core'
-
-import chrome from 'chrome-aws-lambda'
-
-let _page: Page | null
-
-async function getChromeOptions () {
-  return process.env.AWS_REGION
-    ? {
-        args: chrome.args,
-        executablePath: await chrome.executablePath,
-        headless: chrome.headless
-      }
-    : {
-        args: [],
-        executablePath:
-          process.platform === 'win32'
-            ? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
-            : process.platform === 'linux'
-              ? '/usr/bin/google-chrome'
-              : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
-
-      }
-}
+import { connect } from 'puppeteer'
 
 type GetScreenshotParameters = {
   url: string;
 }
 
 export async function getScreenshot ({ url }: GetScreenshotParameters) {
-  const options = await getChromeOptions()
-  const browser = await launch(options)
+  const browser = await connect({
+    browserWSEndpoint: `wss://chrome.browserless.io?token=${process.env.BROWSERLESS_API_KEY}`
+  })
   const page = await browser.newPage()
 
   await page.setViewport({ width: 1000, height: 630 })
