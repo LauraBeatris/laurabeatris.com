0bb993c514e7fed4fd39b32af1273c9771b37b32
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2022-09-21 11:23:18 +0200

    Show skeleton on timeline first load

--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -1,4 +1,4 @@
-import { List, ListItem, VStack } from '@chakra-ui/react'
+import { Box, List, ListItem, Skeleton, SkeletonCircle, SkeletonText, VStack } from '@chakra-ui/react'
 
 import { Heading } from 'components/Base/Heading'
 import { PaginationButton } from 'components/PaginationButton'
@@ -8,8 +8,23 @@ import { useTimelineQuery } from 'hooks/useTimelineQuery'
 
 import { Achievements } from './Achievements'
 
+function TimelineSkeleton () {
+  return (
+    <VStack width='full'>
+      <Box padding='6' boxShadow='lg' bg='white' w='full'>
+        <SkeletonCircle size='10' />
+        <SkeletonText mt='4' noOfLines={4} spacing='4' />
+      </Box>
+      <Box padding='6' boxShadow='lg' bg='white' w='full'>
+        <SkeletonCircle size='10' />
+        <SkeletonText mt='4' noOfLines={4} spacing='4' />
+      </Box>
+    </VStack>
+  )
+}
+
 export function Timeline () {
-  const { timeline, handleNextPage, resetPagination } = useTimelineQuery()
+  const { timeline, isLoading, handleNextPage, resetPagination } = useTimelineQuery()
   const hasNextPage = timeline[timeline?.length - 1]?.hasNextPage
 
   const onPaginationClick = () => {
@@ -31,19 +46,23 @@ export function Timeline () {
         Timeline
       </Heading>
 
-      <List spacing={4}>
-        {
-          (timeline ?? []).map(({ year, achievements }) => (
-            <ListItem key={year}>
-              <Heading size='sm'>
-                {year}
-              </Heading>
+      {isLoading
+        ? <TimelineSkeleton />
+        : (
+          <List spacing={4}>
+            {
+              (timeline ?? []).map(({ year, achievements }) => (
+                <ListItem key={year}>
+                  <Heading size='sm'>
+                    {year}
+                  </Heading>
 
-              <Achievements achievements={achievements} />
-            </ListItem>
-          ))
-        }
-      </List>
+                  <Achievements achievements={achievements} />
+                </ListItem>
+              ))
+            }
+          </List>
+          )}
 
       <PaginationButton
         onClick={onPaginationClick}
